52ef453e81eb152988568e602df353a0dee1e46f
NarutoBratishka <katorabian@gmail.com>
2021-11-23 21:08:25 +0700

    add: onSwipeListener

--- app/src/main/java/com/katorabian/clarcshoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/katorabian/clarcshoplist/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.katorabian.clarcshoplist.R
 import com.katorabian.clarcshoplist.domain.ShopItem
@@ -39,14 +40,31 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        shopListAdapter.apply {
-            onShopItemLongClickListener = {
-                viewModel.changeEnabledState(it)
-            }
+        shopListAdapter.onShopItemLongClickListener = {
+            viewModel.changeEnabledState(it)
+        }
 
-            onShopItemClickListener = {
-                Log.d("qwe: shopItem.onClick", "$it")
+        shopListAdapter.onShopItemClickListener = {
+            Log.d("qwe: shopItem.onClick", "$it")
+        }
+
+        val callback = object: ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean { return false }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = shopListAdapter.shopList[viewHolder.adapterPosition]
+                viewModel.removeShopItem(item)
             }
         }
+
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(rvShopList)
     }
 }
