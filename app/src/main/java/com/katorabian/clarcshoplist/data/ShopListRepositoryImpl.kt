86eabae3be5df9886e88ab86a5015516a24a7114
NarutoBratishka <katorabian@gmail.com>
2022-03-25 02:15:20 +0700

    change: replace MediatorLiveData with Transformations::map

--- app/src/main/java/com/katorabian/clarcshoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/katorabian/clarcshoplist/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.katorabian.clarcshoplist.domain.interfaces.ShopListRepository
 import com.katorabian.clarcshoplist.domain.pojos.ShopItem
 
@@ -31,9 +32,9 @@ class ShopListRepositoryImpl(
         return mapper.mapDbModelToEntity(dbModel)
     }
 
-    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
-        addSource(shopListDao.getShopList()) {
-            value = mapper.mapListDbModelToListEntity(it)
-        }
+    override fun getShopList(): LiveData<List<ShopItem>> = Transformations.map(
+        shopListDao.getShopList()
+    ) {
+        mapper.mapListDbModelToListEntity(it)
     }
 }
