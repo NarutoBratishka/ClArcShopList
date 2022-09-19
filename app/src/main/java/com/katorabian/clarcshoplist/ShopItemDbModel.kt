7a6e5b8ed65c28ec4396ef64d5aa935ec0026d54
NarutoBratishka <katorabian@gmail.com>
2022-09-19 16:10:31 +0700

    create: db for shopItems

---
+++ app/src/main/java/com/katorabian/clarcshoplist/ShopItemDbModel.kt
@@ -0,0 +1,15 @@
+package com.katorabian.clarcshoplist
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shop_items")
+data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    val name: String,
+    val count: Int,
+    val enabled: Boolean
+) {
+
+}
